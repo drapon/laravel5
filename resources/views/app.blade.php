f824b377d7d65394b7a553509b0b18a8bb360bef
drapon <ryuichi.hayashi@gmail.com>
2015-03-10 09:44:54 +0900

    Laravel5インストールから簡易ブログの作成まで

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <title>dev.drapon.com</title>
+      <link href="/css/app.css" rel="stylesheet">
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <h1>dev.drapon.com</h1>
+              <div class="col-md-12">
+                  @yield('content')
+              </div>
+          </div>
+      </div>
+  </body>
+  </html>
